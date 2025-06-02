63b33570906b95721b0e5d7db8220eb50275a952
Regalk <72028266+regalk13@users.noreply.github.com>
2025-06-02 16:30:17 -0500

    docs: improve documentation and remove old build

--- flake.nix
+++ flake.nix
@@ -2,10 +2,10 @@
   description = "Regalk.dev site (Rust + Leptos)";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    crane.url = "github:ipetkov/crane";
-    flake-utils.url = "github:numtide/flake-utils";
-    rust-overlay.url = "github:oxalica/rust-overlay";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable"; # nixpkgs 
+    crane.url = "github:ipetkov/crane"; # cool tool for build rust projects on nix
+    flake-utils.url = "github:numtide/flake-utils"; # flake-utils... yes
+    rust-overlay.url = "github:oxalica/rust-overlay"; # rust overlay for rust 
   };
 
   outputs = { self, nixpkgs, crane, flake-utils, rust-overlay, ... }:
@@ -15,20 +15,20 @@
           inherit system;
           overlays = [ (import rust-overlay) ];
           config.allowUnfree = true;
-        };
+        }; # confg of the pkgs - allowUnFree - and add the rust-overlay 
         craneLib = (crane.mkLib pkgs).overrideToolchain
           (pkgs.rust-bin.nightly.latest.default.override {
             extensions = [ "rust-src" ];
             targets = [ "wasm32-unknown-unknown" ];
-          });
+          }); # tell cranelib to use the specific overlay option
 
-        fullSrc = ./.;
+        fullSrc = ./.; # src of the project
 
-        depSrc = craneLib.cleanCargoSource fullSrc;
+        depSrc = craneLib.cleanCargoSource fullSrc; # load the project and Cargo.toml for deps and build leptos
 
         cargoArtifacts = craneLib.buildDepsOnly {
           src = depSrc;
-        };
+        }; # buildDepsOnly config see https://github.com/ipetkov/crane/issues/291
       in
       {
         packages.default = craneLib.buildPackage {
@@ -43,27 +43,27 @@
             binaryen
             wasm-bindgen-cli
             cargo-leptos
-          ];
+          ]; # pkgs for build the project
 
           buildPhase = ''
             # Set critical environment variables
             export LEPTOS_OUTPUT_NAME=regalk
             export LEPTOS_SITE_ROOT=$PWD/target/site
             cargo-leptos build --release
-          '';
+          ''; # Commands to build leptos app
 
           installPhase = ''
             mkdir -p $out/bin $out/site
             install -m755 target/release/regalk $out/bin/
             cp -r target/site/* $out/site
             cp -r blogs $out/blogs
-          '';
+          ''; # Copy the assets that app itself + blog folder to the $out dir
 
           meta = {
             description = "Rust + Leptos site for regalk.dev";
             mainProgram = "regalk.dev";
             maintainers = [ "regalk" ];
-          };
+          }; # meta stuff
         };
 
         devShells.default = pkgs.mkShell {
@@ -75,9 +75,9 @@
             cargo-leptos
             cargo-generate
           ];
-        };
+        }; # Stuff I use on the `nix develop` shell 
       }
     ) // {
-      nixosModules.default = import ./module.nix self;
+      nixosModules.default = import ./module.nix self; # Module to wrap the app on a systemd service
     };
 }
--- module.nix
+++ module.nix
@@ -11,24 +11,24 @@ in
       default = "0.0.0.0:3000";
       description = "Server listen address";
     };
-  };
+  }; # options to activate the service and address
 
   config = lib.mkIf cfg.enable {
 
     systemd.services.regalk = {
       description = "Regalk website service";
-      after = [ "network.target" ];
-      wantedBy = [ "multi-user.target" ];
+      after = [ "network.target" ]; # ensure netwrok is up
+      wantedBy = [ "multi-user.target" ]; # multi-user setup
       serviceConfig = {
-        ExecStart = "${sitePkg}/bin/regalk";
+        ExecStart = "${sitePkg}/bin/regalk"; # bin
         Environment = [
           "LEPTOS_SITE_ROOT=${sitePkg}/site"
           "LEPTOS_SITE_ADDR=${cfg.address}"
           "BLOGS_DIR=${sitePkg}/blogs"
-        ];
+        ]; # env the app uses
         Restart = "always";
         PrivateTmp = true;
       };
-    };
+    }; # default service regalk that will run the leptos page
   };
 }
